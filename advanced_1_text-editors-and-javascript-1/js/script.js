5a0db39951a3cedba9a48a0d2e225d25cad943b2
Dražen Lučanin <kermit666@gmail.com>
2020-10-08 18:56:50 +0200

    add lesson 1 materials

---
+++ advanced_1_text-editors-and-javascript-1/js/script.js
@@ -0,0 +1,33 @@
+// alert("hello world");
+
+var greeting = 'hello wide world';
+console.log(greeting);
+greeting = "Bok, svijete. Dobar dan!";
+console.log(greeting);
+
+var a = 26;
+ 
+var b = 27;
+
+console.log(a + b);
+
+first_name = "Ivo";
+last_name = "Peric";
+console.log(first_name + last_name);
+console.log(first_name + " " + last_name);
+console.log(`${first_name} ${last_name}`);
+
+
+var sum = a + b * 7;
+console.log(sum);
+
+var temperature = 35.6;
+console.log(temperature);
+
+function add(a, b) {
+  var sum = a + b;
+  return sum;
+}
+
+var result_of_a_function = add(17, 8);
+console.log(result_of_a_function);
---
+++ advanced_1_text-editors-and-javascript-1/texteditor.md
@@ -0,0 +1,38 @@
+# Atom
+
+Download from [Atom](https://atom.io).
+
+## Useful keyboard shortcuts
+
+```
+ctrl+p - fuzzy open
+cmd+p
+```
+
+ctrl+shift+p - run command
+Cmd+shift+p
+
+ctrl+f - find in file
+cmd+f
+
+ctrl+shift+f - find in project
+cmd+shift+f
+
+ctrl+o – jump between file editor and file tree
+ctrl+o
+a - add new file
+shift-a - add new folder
+
+## Editing files
+
+- ctrl + / - comment selected code
+
+## keyboard layouts
+
+US layout the most common for programming
+
+[Finger map](https://apus.tdksoft.co.uk/typing/touch-typing.html)
+
+packages, for example:
+
+- [prettier](https://prettier.io/)
