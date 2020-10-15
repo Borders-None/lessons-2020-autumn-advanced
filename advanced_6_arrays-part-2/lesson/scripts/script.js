a5c8b504a1919c147638474ebfe396c8790f9e81
Josip Grgic <grgicjosip310@gmail.com>
2020-10-15 20:47:41 +0200

    Some exercises we did in the class

--- advanced_6_arrays-part-2/lesson/scripts/script.js
+++ advanced_6_arrays-part-2/lesson/scripts/script.js
@@ -1,9 +1,58 @@
-var temperature = 29;
+function printme(number) {
+    let newNumber = number * 10;
+    let newernumber = newNumber + 15;
 
-if (temperature < 0) {
-    console.log("Cold");
-} else if (temperature >= 0 && temperature < 30) {
-    console.log("OK");
-} else {
-    console.log("Hot");
+    console.log(newernumber);
 }
+
+///printme(15);
+/**
+ *  let newNumber = 15 * 10;
+    let newernumber = newNumber + 15;
+
+    console.log(newernumber);
+ **/
+
+//printme(47);
+/**
+ *  let newNumber = 47 * 10;
+    let newernumber = newNumber + 15;
+
+    console.log(newernumber);
+ **/
+
+//printme(100);
+
+/*let sprintTimes = [12.0, 11.9, 11.89, 11.5, 13, 15];
+
+sprintTimes.sort();
+
+for (let i = 0; i < sprintTimes.length; i++) {
+    console.log(i + 1 + ". place: " + sprintTimes[i]);
+}*/
+
+function getFirstName() {
+    return "Linus";
+}
+
+function getLastName() {
+    return "Torvalds";
+}
+
+function printGreeting(firstNameFunction, lastNameFunction) {
+    const firstName = firstNameFunction();
+
+    const lastName = lastNameFunction();
+
+    const greeting = `Hello ${firstName} ${lastName}.`;
+    console.log(greeting);
+}
+
+function printGreeting2(firstName, lastName) {
+    const greeting = `Hello ${firstName} ${lastName}`;
+    console.log(greeting);
+}
+
+printGreeting(getFirstName, getLastName);
+printGreeting(getLastName, getFirstName);
+printGreeting2(getLastName(), getFirstName());
--- advanced_6_arrays-part-2/recap/scripts/script.js
+++ advanced_6_arrays-part-2/recap/scripts/script.js
@@ -1,4 +1,4 @@
-recap1();
+recap4();
 
 /** Recap 1 - arrays
  * Let's create an array of 5 elements
@@ -6,26 +6,61 @@ recap1();
  * and print out each element separetly
  */
 
-function recap1() {}
+function recap1() {
+    const array = [1, 2, 3, 4, 5, 6, 7];
+
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+}
 
 /** Recap 2 - arrays
  * Let's create 2 arrays of 5 numbers
  * and create a third array as a sum of the first two
  */
 
-function recap2() {}
+function recap2() {
+    const array1 = [1, 2, 3, 4, 5];
+    const array2 = [4, 2, 9, 5, 6];
+    const array3 = []; // 5, 4, 12, 9, 11
+
+    let i = 0;
+
+    while (i < array1.length) {
+        array3[i] = array1[i] + array2[i];
+        i++;
+    }
+
+    console.log(array3);
+}
 
 /** Recap 3 - functions
  * Lets create a new year countdown from 10 do 0 the bad way
  */
 
-function recap3() {}
+function recap3() {
+    console.log(10);
+    console.log(9);
+    console.log(8);
+    console.log(7);
+    console.log(6);
+    console.log(6);
+    console.log(4);
+    console.log(3);
+    console.log(2);
+    console.log(1);
+    console.log(0);
+}
 
 /** Recap 4 - functions
  * Lets create a new year countdown from 10 do 0 the good way
  */
 
-function recap4() {}
+function recap4() {
+    for (i = 100; i >= 0; i--) {
+        console.log(i);
+    }
+}
 
 /** Recap 4 - functions
  * Lets create a new year countdown from 10 do 0 the strange way
