5f4f0292ad972d990f38aab01b8277e542f971df
Josip Grgic <grgicjosip310@gmail.com>
2020-11-19 02:02:15 +0100

    Created small local REST API

---
+++ advanced_15_HTTP_API/emails_api/db.js
@@ -0,0 +1,43 @@
+const emails = [
+  {
+    id: 1,
+    from: "Nataša",
+    title: "Extra materials for learning",
+    body: "Hey everyone, I am sending you the materials.",
+  },
+  {
+    id: 2,
+    from: "State lottery",
+    title: "Congratulations, you've won!",
+    body: "WOW! You did it! You won! So much money!",
+  },
+  {
+    id: 3,
+    from: "Mladen",
+    title: "Don't forget your homework!",
+    body: "hey, do not forget your homework for Tuesday!",
+  },
+  {
+    id: 4,
+    from: "ACME",
+    title: "Job interview",
+    body:
+      "When do you have time to stop by for an interview? Would next week work for you?",
+  },
+  {
+    id: 5,
+    from: "Josip",
+    title: "Practice makes perfect!",
+    body:
+      "I just wanted to say – please try out those examples we talked about!",
+  },
+  {
+    id: 6,
+    from: "Dražen",
+    title: "Next class will be about...",
+    body:
+      "Hello, in the next class we will learn exciting new things about ReactJS!",
+  },
+];
+
+export default emails;
